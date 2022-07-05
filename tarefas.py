9715b013d630a4285b8f0179fec64e74eed50720
Denis Oliveira <denisbob21@gmail.com>
2022-07-05 17:10:24 -0300

    Acrescentado nota do trabalho na média

--- tarefas.py
+++ tarefas.py
@@ -3,9 +3,11 @@
 prova1: float
 prova2: float
 media: float
+trabalho: float
 
 prova1 = float(input("Informe a nota da primeira prova: "))
 prova2 = float(input("Informe a nota da segunda prova: "))
+trabalho = float(input("Informe a nota do trabalho: "))
 
-media = (prova1 + prova2) / 2
+media = (prova1 + prova2 + trabalho) / 3
 print(f"Sua média é: {media:,.2f}")
